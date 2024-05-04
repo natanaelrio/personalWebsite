c1b7bee479ffdf6802fe63e83a33233c86453c47
Natanael Rio Wijaya <gafterlife@gmail.com>
2024-05-04 13:49:23 +0700

    v2

--- src/app/component/projects.jsx
+++ src/app/component/projects.jsx
@@ -1,4 +1,5 @@
 import styles from '@/app/component/projects.module.css'
+import Image from 'next/image'
 
 export default function Project() {
     return (
@@ -6,12 +7,17 @@ export default function Project() {
             <div className='judul'>Project</div>
             <div className={styles.kotak}>
                 <div className={styles.kotak1}>
-                    <div className={styles.gambar}></div>
+                    <div className={styles.gambar}>
+                        <Image src={`${process.env.NEXT_PUBLIC_URL}/personalWebsite.png`} alt='personalwebsite' width={500} height={350} ></Image>
+                    </div>
                     <div className={styles.judul}>Personal Website</div>
                     <div className={styles.deskripsi}>Situs web ini berfungsi sebagai wadah untuk portofolio saya, menampilkan proyek-proyek terbaru yang saya kerjakan.</div>
                 </div>
                 <div className={styles.kotak1}>
-                    <div className={styles.gambar}></div>
+                    <div className={styles.gambar}>
+                        <Image src={`${process.env.NEXT_PUBLIC_URL}/decolofit.png`} alt='decolofit' width={500} height={350} ></Image>
+
+                    </div>
                     <div className={styles.judul}>Decolofit</div>
                     <div className={styles.deskripsi}>Situs web e-commerce pribadi saya didesain untuk menyajikan produk-produk yang saya tawarkan kepada pelanggan secara online.</div>
                 </div>
