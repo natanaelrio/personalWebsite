a94f9b13e6824ec07718ff21c7273b9328b76dab
Natanael Rio Wijaya <gafterlife@gmail.com>
2024-05-19 00:18:42 +0700

    v3

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -5,15 +5,35 @@ import { FaGithub } from "react-icons/fa";
 import { FaSquareWhatsapp } from "react-icons/fa6";
 import { FaInstagramSquare } from "react-icons/fa";
 import { IoIosArrowDropup } from "react-icons/io";
+import { useEffect, useState } from 'react';
 
-export default function FooterPage({ view }) {
+export default function FooterPage() {
     const handleOnTop = () => {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
     }
+
+    const [view, setView] = useState([])
+    useEffect(() => {
+        const fetchdata = async () => {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/get-viewproduct?id=1`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': process.env.NEXT_PUBLIC_SECREET
+                },
+                next: { revalidate: 0 }
+            })
+            const data = await res.json()
+            setView(data)
+        }
+        fetchdata()
+    }, [])
+
+    const TotalView = view.data[0].view_barang
     return (
         <footer className={styles.footer}>
-            <div className={styles.kiri}>© with ❤️ Natanael Rio Wijaya ({view})</div>
+            <div className={styles.kiri}>© with ❤️ Natanael Rio Wijaya ({TotalView})</div>
             <div className={styles.kanan}>
                 <Link href={'https://github.com/natanaelrio'} >
                     <FaInstagramSquare size={30} className={styles.icon} />
--- src/component/layoutHeaderFooter.jsx
+++ src/component/layoutHeaderFooter.jsx
@@ -2,13 +2,13 @@ import styles from '@/component/layoutHeaderFooter.module.css'
 import HeaderPage from "@/component/header";
 import FooterPage from '@/component/footer';
 
-export default function LayoutHeaderFooter({ children, view }) {
+export default function LayoutHeaderFooter({ children }) {
     return (
         <main className={styles.main}>
             <div className={styles.isi}>
                 <HeaderPage />
                 {children}
-                <FooterPage view={view} />
+                <FooterPage />
             </div>
         </main>
     )
