b9a411a96c4cfd1662b175058d42c24d8f48b2dc
Natanael Rio Wijaya <gafterlife@gmail.com>
2024-05-19 00:06:36 +0700

    v3

--- src/app/page.js
+++ src/app/page.js
@@ -4,30 +4,30 @@ import ProfilPage from "@/component/profil";
 import ScroolDown from "@/component/scrooldown";
 import Skill from "@/component/skill";
 import Project from "@/component/projects";
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-dynamic'
 
 export const metadata = {
   title: 'Natanael Rio Wijaya',
   description: 'Portofolio Natanael Rio Wijaya',
 }
-export async function AmbilView() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/get-viewproduct?id=1`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': process.env.NEXT_PUBLIC_SECREET
-    },
-    next: { revalidate: 0 }
-  })
+// export async function AmbilView() {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/get-viewproduct?id=1`, {
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Authorization': process.env.NEXT_PUBLIC_SECREET
+//     },
+//     next: { revalidate: 0 }
+//   })
 
-  return res.json()
-
-}
+//   return res.json()
+// }
 
 export default async function Home() {
-  const dataID = await AmbilView()
+  // const dataID = await AmbilView()
+  // dataID.data[0].view_barang
   return (
-    <LayoutHeaderFooter view={dataID.data[0].view_barang}>
+    <LayoutHeaderFooter view={1}>
       <ProfilPage />
       <ScroolDown />
       <Skill />
